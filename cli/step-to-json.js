3dbc181b946161b377e5f0b9ea6c525da7bd1e75
aljoshakoecher <50097079+aljoshakoecher@users.noreply.github.com>
2020-01-29 09:01:05 +0100

    Fixed a couple of var and let declarations

--- cli/step-to-json.js
+++ cli/step-to-json.js
@@ -1,9 +1,8 @@
-// TODO: Fix vars and lets
 const Subject = require("rxjs").Subject;
 
 // relevant imports
-var fs = require("fs");
-var colors = require('colors');
+const fs = require("fs");
+const colors = require('colors');
 const yargs = require("yargs/yargs");
 const cliProgress = require('cli-progress');
 const StepToJsonParser = require('./../lib/parser');
@@ -12,22 +11,22 @@ const parser = new StepToJsonParser();
 
 
 // variables for preprocessed content
-var FILE_NAME;
-var FILE_SCHEMA;
-var FILE_DESCRIPTION;
-var PRODUCT_DEFINITIONS = [];
-var NEXT_ASSEMBLY_USAGE_OCCURRENCE = [];
+let FILE_NAME;
+let FILE_SCHEMA;
+let FILE_DESCRIPTION;
+let PRODUCT_DEFINITIONS = [];
+let NEXT_ASSEMBLY_USAGE_OCCURRENCE = [];
 
 // preprocessed object
 let step = {
     header: {
-        FILE_DESCRIPTION: FILE_DESCRIPTION,
-        FILE_NAME: FILE_NAME,
-        FILE_SCHEMA: FILE_SCHEMA,
+        "FILE_DESCRIPTION": FILE_DESCRIPTION,
+        "FILE_NAME": FILE_NAME,
+        "FILE_SCHEMA": FILE_SCHEMA,
     },
     data: {
-        PRODUCT_DEFINITION: PRODUCT_DEFINITIONS,
-        NEXT_ASSEMBLY_USAGE_OCCURRENCE: NEXT_ASSEMBLY_USAGE_OCCURRENCE,
+        "PRODUCT_DEFINITION": PRODUCT_DEFINITIONS,
+        "NEXT_ASSEMBLY_USAGE_OCCURRENCE": NEXT_ASSEMBLY_USAGE_OCCURRENCE,
     }
 }
 
@@ -46,9 +45,11 @@ console.time("Elapsed time")
 
 // read the file and split lines by ";"
 console.log("Reading the specified file ...".yellow)
+
+let lines;
 try {
-    var file = fs.readFileSync(argv.filename);
-    var lines = file.toString().split(";")
+    const file = fs.readFileSync(argv.filename);
+    lines = file.toString().split(";")
 } catch (error) {
     if (error.code == "ENOENT") {
         console.error("Specified file could not be found...".red)
@@ -57,14 +58,14 @@ try {
 }
 
 // preprocess the content by tag
-let preBar = new cliProgress.SingleBar({
+const preBar = new cliProgress.SingleBar({
     format: 'Preprocessing the step file \t|' + colors.cyan('{bar}') + '| {percentage}% || {value}/{total} Lines',
     barCompleteChar: '\u2588',
     barIncompleteChar: '\u2591',
     hideCursor: true
 });
 preBar.start(lines.length, 0);
-// TODO: Loop can be stopped when all attributes are found...
+
 for (let i = 0; i < lines.length; i++) {
     preBar.increment();
     let lineString = lines[i];
@@ -83,23 +84,23 @@ for (let i = 0; i < lines.length; i++) {
 preBar.stop();
 
 // get relations and products
-let bar = new cliProgress.SingleBar({
+const relationsBar = new cliProgress.SingleBar({
     format: 'Parsing relations \t\t|' + colors.cyan('{bar}') + '| {percentage}% || {value}/{total} Relations',
     barCompleteChar: '\u2588',
     barIncompleteChar: '\u2591',
     hideCursor: true
 });
-bar.start(step.data.NEXT_ASSEMBLY_USAGE_OCCURRENCE.length, 0);
+relationsBar.start(step.data.NEXT_ASSEMBLY_USAGE_OCCURRENCE.length, 0);
 const assemblyUsageSubject = new Subject();
 assemblyUsageSubject.subscribe({
-    next:       (data) =>  {bar.update(data)},
-    complete:   () => {bar.stop()}
+    next:       (data) =>  {relationsBar.update(data)},
+    complete:   () => {relationsBar.stop()}
 });
     
-var relations = parser.parse_NEXT_ASSEMBLY_USAGE_OCCURRENCE(step.data.NEXT_ASSEMBLY_USAGE_OCCURRENCE, assemblyUsageSubject);
+const relations = parser.parse_NEXT_ASSEMBLY_USAGE_OCCURRENCE(step.data.NEXT_ASSEMBLY_USAGE_OCCURRENCE, assemblyUsageSubject);
 
 
-let productBar = new cliProgress.SingleBar({
+const productBar = new cliProgress.SingleBar({
     format: 'Parsing products \t\t|' + colors.cyan('{bar}') + '| {percentage}% || {value}/{total} Chunks',
     barCompleteChar: '\u2588',
     barIncompleteChar: '\u2591',
@@ -113,14 +114,14 @@ productDefinitionSubject.subscribe({
     complete:   () => {productBar.stop()}
 });
 
-var products = parser.parse_PRODUCT_DEFINITION(step.data.PRODUCT_DEFINITION, productDefinitionSubject);
+const products = parser.parse_PRODUCT_DEFINITION(step.data.PRODUCT_DEFINITION, productDefinitionSubject);
 
-var rootAssemblyObject = {}
+let rootAssemblyObject = {}
 
 // identify rootAssemblyKey
 products.forEach(element => {
-    let productKey = element.key;
-    let productName = element.name;
+    const productKey = element.key;
+    const productName = element.name;
     for (let i = 0; i < relations.length; i++) {
 
         if (relations[i].contains == productKey) {
@@ -135,10 +136,10 @@ products.forEach(element => {
 });
 
 // build first level assembly object
-var assemblyObject = parser.buildStructureObject(rootAssemblyObject);
+const assemblyObject = parser.buildStructureObject(rootAssemblyObject);
 
 // add recursively to assembly object
-let buildBar = new cliProgress.SingleBar({
+const buildBar = new cliProgress.SingleBar({
     format: 'Building the output \t\t|' + colors.cyan('{bar}') + '| {percentage}% || {value}/{total} Chunks',
     barCompleteChar: '\u2588',
     barIncompleteChar: '\u2591',
